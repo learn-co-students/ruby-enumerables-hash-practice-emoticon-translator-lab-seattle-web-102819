f7c95e90c378a82f7107da62d43cf0aafb3489ff
Amy C <missch3n@gmail.com>
2019-10-27 04:29:18 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,19 +11,19 @@ def load_library(file)
   result
 end
 
-def get_japanese_emoticon(file,english_emoji)
+def get_japanese_emoticon(file,english_emoticon)
   library = load_library(file)
-  if library["get_emoticon"].include?(english_emoji)
-    return library["get_emoticon"][english_emoji]
+  if library["get_emoticon"].include?(english_emoticon)
+    return library["get_emoticon"][english_emoticon]
   else
     return "Sorry, that emoticon was not found"
   end
 end
 
-def get_english_meaning(file,japanese_emoji)
+def get_english_meaning(file,japanese_emoticon)
   library = load_library(file)
-  if library["get_meaning"].include?(japanese_emoji)
-    return library["get_meaning"][japanese_emoji]
+  if library["get_meaning"].include?(japanese_emoticon)
+    return library["get_meaning"][japanese_emoticon]
   else
     return "Sorry, that emoticon was not found"
   end
